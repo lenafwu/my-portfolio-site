3fab039887aa27bfc326760232766aa0135dd73f
LenaFWU <lena.f.wu@gmail.com>
2023-06-03 18:41:56 -0400

    added project view

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,30 @@ router.get("/", function (req, res, next) {
 router.get("/about", function (req, res, next) {
   res.render("index", { title: "About" });
 });
+
+router.get("/project", function (req, res, next) {
+  res.render("projects", projects);
+});
+
+const projects = {
+  title: "Projects",
+  projects: [
+    {
+      name: "Project 1",
+      description: "This is the first project",
+      link: "www.example.com",
+    },
+    {
+      name: "Project 2",
+      description: "This is the second project",
+      link: "www.example.com",
+    },
+    {
+      name: "Project 3",
+      description: "This is the third project",
+      link: "www.example.com",
+    },
+  ],
+};
+
 module.exports = router;
